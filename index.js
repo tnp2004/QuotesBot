f755db2d2658db72b3deb00af32f713c232549ac
tnp2004 <iceandiceisice2004@gmail.com>
2022-08-21 14:42:56 +0700

    QuoteBot Released

---
+++ index.js
@@ -0,0 +1,77 @@
+const { Client, GatewayIntentBits, SlashCommandStringOption } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const dotenv = require('dotenv');
+dotenv.config();
+const axios = require("axios");
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+
+let quote;
+let author;
+let contentTH;
+let contentEN;
+
+const options = {
+  method: 'GET',
+  url: 'https://quotes15.p.rapidapi.com/quotes/random/',
+  headers: {
+    'X-RapidAPI-Key': 'b1d71f2a47msh354b953e64b9763p1a6a44jsn12a815718d9b',
+    'X-RapidAPI-Host': 'quotes15.p.rapidapi.com'
+  }
+};
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+const quoteRequest = () => {
+    axios.request(options).then(function (response) {
+        quote = response.data.content
+        author = response.data.originator.name
+    }).catch(function (error) {
+        console.error(error);
+    });
+}
+
+function translate(sentences, from_lang ='en', to_lang='th'){
+  
+  let endPoint = `https://translate.googleapis.com/translate_a/single?client=gtx&sl=${from_lang}&tl=${to_lang}&dt=t&ie=UTF-8&oe=UTF-8&q=${encodeURIComponent(sentences)}` ;
+						
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+		var jsonText = JSON.parse(this.responseText);
+		contentTH = jsonText[0][0][0]
+    }
+  };
+  xhttp.open("GET", endPoint, true);
+  xhttp.send();
+}
+
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+  quoteRequest();
+
+  if (interaction.commandName === 'ping') {
+    await interaction.reply('Pong!');
+  }
+
+  if (interaction.commandName === 'quote') {
+   if (quote ) {
+      contentEN = `>>> :scroll: **${quote}** - __${author}__`;
+      await interaction.reply(contentEN);
+      return;
+   }
+   await interaction.reply(">>> Whoops! Something went wrong. Please try again later.");
+  }
+
+  if (interaction.commandName === 'quoteแปลด้วยนะ') {
+    translate(quote);
+   if (quote && contentTH) {
+      await interaction.reply(`>>> :scroll: **${contentTH}** - __${author}__`);
+      return
+   }
+   await interaction.reply(`>>> **มีบางอย่างผิดพลาด โปรดลองอีกครั้งในภายหลัง**`);
+  }
+});
+
+client.login(process.env.TOKEN);
---
+++ register.js
@@ -0,0 +1,35 @@
+const { REST, Routes } = require('discord.js');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const commands = [
+  {
+    name: 'ping',
+    description: 'Replies with Pong!',
+  },
+
+  {
+    name : 'quote',
+    description: "Replies with a quote",
+  },
+
+  {
+    name : 'quoteแปลด้วยนะ',
+    description: "Replies with a quote และ คำแปล",
+  }
+];
+
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
+(async () => {
+  try {
+    console.log('Started refreshing application (/) commands.');
+
+    await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: commands });
+
+    console.log('Successfully reloaded application (/) commands.');
+  } catch (error) {
+    console.error(error);
+  }
+})();
